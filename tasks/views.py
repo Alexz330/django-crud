924c2bbc2bf2960564c5c2c19f7d1e91e9d20a97
alexz330 <alexislc330@gmail.com>
2022-11-12 23:04:53 -0600

    agrengando validacion de errores a formulario de signup

--- tasks/views.py
+++ tasks/views.py
@@ -1,24 +1,28 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse 
+from django.http import HttpResponse
+
+
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
+
 
 def signup(request):
-    dic = {'form': UserCreationForm}
 
     if request.method == 'GET':
-        return render(request,'signup.html',dic)
+        return render(request, 'signup.html', {'form': UserCreationForm})
     else:
 
-        if request.POST['password1'] == request.POST['password']:
+        if request.POST['password1'] == request.POST['password2']:
             try:
-                username =  request.POST["username"]
-                password =  request.POST["password"]
-                user = User.objects.create_user(username=username,password=password)
+                username = request.POST["username"]
+                password = request.POST["password1"]
+                user = User.objects.create_user(
+                    username=username, password=password)
                 user.save()
+                return HttpResponse('Usuario created successfully')
+
             except:
-                return HttpResponse('Username already exists')
-            return HttpResponse('Usuario created successfully')
-        return HttpResponse('Password do not match')
+                return render(request, 'signup.html', {'form': UserCreationForm, "error": "usernaem already exist"})
+        return render(request, 'signup.html', {'form': UserCreationForm, "error": "password do not match"})
