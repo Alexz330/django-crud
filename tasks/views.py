2176a500db87024c9f3a598a41e65da83dd98ee4
alexz330 <alexislc330@gmail.com>
2022-11-12 22:44:05 -0600

    agregando formulario de registro para usuario

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse 
+def home(request):
+    return render(request,'home.html')
 
-# Create your views here.
+def signup(request):
+    dic = {'form': UserCreationForm}
+
+    if request.method == 'GET':
+        return render(request,'signup.html',dic)
+    else:
+
+        if request.POST['password1'] == request.POST['password']:
+            try:
+                username =  request.POST["username"]
+                password =  request.POST["password"]
+                user = User.objects.create_user(username=username,password=password)
+                user.save()
+            except:
+                return HttpResponse('Username already exists')
+            return HttpResponse('Usuario created successfully')
+        return HttpResponse('Password do not match')
