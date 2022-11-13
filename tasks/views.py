df8834913193ebfcff557cb27f17f972190ae6bf
alexz330 <alexislc330@gmail.com>
2022-11-13 11:36:41 -0600

    creando modelo de [Tasks]

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponse
 from django.db import IntegrityError
+
 
 def home(request):
     return render(request, 'home.html')
@@ -18,23 +19,52 @@ def signup(request):
         if request.POST['password1'] == request.POST['password2']:
 
             try:
-                         
+
                 username = request.POST["username"]
                 password = request.POST["password1"]
                 user = User.objects.create_user(
                     username=username, password=password)
                 user.save()
-                login(request=request,user=user)
+                login(request=request, user=user)
                 return redirect('tasks')
-            
+
             except IntegrityError:
-                return render(request, 'signup.html', {'form': UserCreationForm, "error": "user already exists"})        
-            
+                return render(request, 'signup.html', {'form': UserCreationForm, "error": "user already exists"})
+
         return render(request, 'signup.html', {'form': UserCreationForm, "error": "password do not match"})
 
+
 def tasks(request):
-    return render(request,'tasks.html')
+    return render(request, 'tasks.html')
+
 
 def signout(request):
     logout(request)
     return redirect('home')
+
+
+def signin(request):
+
+    if request.method == "GET":
+        return render(request, 'signin.html', {
+            "form": AuthenticationForm
+
+        })
+    else:
+        print(request.POST)
+        user = authenticate(
+            request=request,
+            username=request.POST['username'],
+            password=request.POST['password']
+        )
+
+        if user is None:
+            print("no encontrado")
+            return render(request, 'signin.html',{
+                'form':AuthenticationForm,
+                'error':"Username or password is incorrect"
+           
+            })
+        else:
+            login(request=request,user=user)
+            return redirect('tasks')
